c9cd10eb795a4824860d810dad95c45ad166e16c
Jeff Mickey <jm@igneous.io>
2014-11-12 16:03:01 -0800

    Cache results in xdg, so no reasks

--- yosumiru.go
+++ yosumiru.go
@@ -3,13 +3,28 @@ package main
 
 import (
 	"bufio"
+	"crypto/sha1"
+	"encoding/json"
 	"encoding/xml"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 	"os"
 	"os/exec"
+	"path/filepath"
+	"sync/atomic"
 )
+
+var CacheFile string
+
+func init() {
+	xch := os.Getenv("XDG_CACHE_HOME")
+	if xch == "" {
+		home := os.Getenv("HOME")
+		xch = filepath.Join(home, ".cache")
+	}
+	CacheFile = filepath.Join(xch, "yosumiru", "feeds_seen.json")
+}
 
 func main() {
 	if LastFeeds() {
@@ -26,66 +41,21 @@ func RunUpdate() {
 	cmd.Run()
 }
 
-// private wrapper around the RssFeed which gives us the <rss>..</rss> xml
 type rssFeedXml struct {
 	XMLName xml.Name `xml:"rss"`
-	Version string   `xml:"version,attr"`
 	Channel *RssFeed
 }
 
-type RssImage struct {
-	XMLName xml.Name `xml:"image"`
-	Url     string   `xml:"url"`
-	Title   string   `xml:"title"`
-	Link    string   `xml:"link"`
-	Width   int      `xml:"width,omitempty"`
-	Height  int      `xml:"height,omitempty"`
-}
-
-type RssTextInput struct {
-	XMLName     xml.Name `xml:"textInput"`
-	Title       string   `xml:"title"`
-	Description string   `xml:"description"`
-	Name        string   `xml:"name"`
-	Link        string   `xml:"link"`
-}
-
 type RssFeed struct {
-	XMLName        xml.Name `xml:"channel"`
-	Title          string   `xml:"title"`       // required
-	Link           string   `xml:"link"`        // required
-	Description    string   `xml:"description"` // required
-	Language       string   `xml:"language,omitempty"`
-	Copyright      string   `xml:"copyright,omitempty"`
-	ManagingEditor string   `xml:"managingEditor,omitempty"` // Author used
-	WebMaster      string   `xml:"webMaster,omitempty"`
-	PubDate        string   `xml:"pubDate,omitempty"`       // created or updated
-	LastBuildDate  string   `xml:"lastBuildDate,omitempty"` // updated used
-	Category       string   `xml:"category,omitempty"`
-	Generator      string   `xml:"generator,omitempty"`
-	Docs           string   `xml:"docs,omitempty"`
-	Cloud          string   `xml:"cloud,omitempty"`
-	Ttl            int      `xml:"ttl,omitempty"`
-	Rating         string   `xml:"rating,omitempty"`
-	SkipHours      string   `xml:"skipHours,omitempty"`
-	SkipDays       string   `xml:"skipDays,omitempty"`
-	Image          *RssImage
-	TextInput      *RssTextInput
-	Items          []*RssItem `xml:"item"`
+	XMLName xml.Name   `xml:"channel"`
+	Items   []*RssItem `xml:"item"`
 }
 
 type RssItem struct {
 	XMLName     xml.Name `xml:"item"`
-	Title       string   `xml:"title"`       // required
-	Link        string   `xml:"link"`        // required
-	Description string   `xml:"description"` // required
-	Author      string   `xml:"author,omitempty"`
-	Category    string   `xml:"category,omitempty"`
-	Comments    string   `xml:"comments,omitempty"`
-	Enclosure   *RssEnclosure
-	Guid        string `xml:"guid,omitempty"`    // Id used
-	PubDate     string `xml:"pubDate,omitempty"` // created or updated
-	Source      string `xml:"source,omitempty"`
+	Title       string   `xml:"title"`
+	Description string   `xml:"description"`
+	PubDate     string   `xml:"pubDate,omitempty"`
 }
 
 type RssEnclosure struct {
@@ -97,7 +67,68 @@ type RssEnclosure struct {
 
 const ARCH_NEWS_FEED = "https://www.archlinux.org/feeds/news/"
 
-func LastFeeds() bool {
+type FeedsSeen struct {
+	SeenMap map[string]struct{} `json:"seen_map"`
+}
+
+type Entry struct {
+	Title       string
+	PubDate     string
+	Description string
+	Hash        string
+}
+
+func (e *Entry) HashIt() {
+	var entryb []byte
+	entryb = append(entryb, []byte(e.Title)...)
+	entryb = append(entryb, []byte(e.PubDate)...)
+	entryb = append(entryb, []byte(e.Description)...)
+
+	e.Hash = fmt.Sprintf("%x", sha1.Sum(entryb))
+}
+
+func (e *Entry) Print() {
+	fmt.Printf(`* %s
+[%s]
+%s
+-----
+
+`, e.Title, e.PubDate, fixDesc(e.Description))
+}
+
+func checkUser(question string) bool {
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Print(question + " [Y/n]: ")
+	text, _ := reader.ReadString('\n')
+	return (text == "\n" ||
+		text == "Y\n" ||
+		text == "y\n")
+}
+
+func GetFeedsSeen(fsc chan *FeedsSeen) {
+	var fs FeedsSeen
+
+	cache, err := ioutil.ReadFile(CacheFile)
+	if err != nil {
+		_, ok := err.(*os.PathError)
+		if !ok {
+			panic(err)
+		}
+	} else {
+		err = json.Unmarshal(cache, &fs)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	if fs.SeenMap == nil {
+		fs.SeenMap = make(map[string]struct{}, 0)
+	}
+
+	fsc <- &fs
+}
+
+func GetArchFeed(rc chan *rssFeedXml) {
 	resp, err := http.Get(ARCH_NEWS_FEED)
 	if err != nil {
 		panic(err)
@@ -115,31 +146,76 @@ func LastFeeds() bool {
 		fmt.Printf("%v\n%#v\n", err, err)
 	}
 
-	// print most recent 2 items
-	for i := 2; i > 0; i-- {
-		fmt.Printf(`* %s
-[%s]
-%s
------
+	rc <- &res
+}
 
-`, res.Channel.Items[i].Title, res.Channel.Items[i].PubDate, fixDesc(res.Channel.Items[i].Description))
-		reader := bufio.NewReader(os.Stdin)
-		fmt.Print("Given the post, continue? [Y/n]: ")
-		text, _ := reader.ReadString('\n')
-		if text == "\n" ||
-			text == "Y\n" ||
-			text == "y\n" {
-			continue
-		}
-
+func SaveCache(f *FeedsSeen) {
+	b, err := json.Marshal(*f)
+	if err != nil {
+		panic(err)
 	}
 
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Continue with update? [Y/n]: ")
-	text, _ := reader.ReadString('\n')
-	return (text == "\n" ||
-		text == "Y\n" ||
-		text == "y\n")
+	err = os.MkdirAll(filepath.Dir(CacheFile), 0700)
+	if err != nil {
+		panic(err)
+	}
+
+	err = ioutil.WriteFile(CacheFile, b, 0600)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func LastFeeds() bool {
+	feeds := make(chan *rssFeedXml, 0)
+	caches := make(chan *FeedsSeen, 0)
+
+	go GetFeedsSeen(caches)
+	go GetArchFeed(feeds)
+
+	feed := <-feeds
+	cache := <-caches
+
+	entries := make([]*Entry, 0)
+	// copy & hash records
+	for _, v := range feed.Channel.Items {
+		entries = append(entries, &Entry{
+			Title:       v.Title,
+			PubDate:     v.PubDate,
+			Description: v.Description,
+		})
+	}
+
+	// hash entries
+	count := new(int64)
+	done := make(chan struct{}, 0)
+	for _, v := range entries {
+		go func() {
+			v.HashIt()
+			if atomic.AddInt64(count, 1) == int64(len(entries)) {
+				done <- struct{}{}
+			}
+		}()
+	}
+	<-done
+
+	// print unseen records
+	for _, v := range entries {
+		_, ok := cache.SeenMap[v.Hash]
+		if ok {
+			continue
+		}
+		v.Print()
+		if checkUser("Confirm seen?") {
+			cache.SeenMap[v.Hash] = struct{}{}
+		}
+	}
+
+	c := checkUser("Continue with update?")
+	if c {
+		SaveCache(cache)
+	}
+	return c
 }
 
 func fixDesc(desc string) string {
